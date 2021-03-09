9b18386cac2b1e481bfdac1498eef4d0393648b4
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2021-03-08 19:00:23 -0500

    MVP + Stretch
    Gave up on formatting to currency, but finished all other goals.

---
+++ src/vendingapp/Vending.java
@@ -0,0 +1,23 @@
+package vendingapp;
+
+public class Vending{
+	private static int maxId=0;
+	private int id;
+	private String name;
+
+	public Vending(String name){
+		maxId++;
+		id=maxId;
+		this.name=name;
+	}
+
+	public int getId(){
+		return id;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+}
